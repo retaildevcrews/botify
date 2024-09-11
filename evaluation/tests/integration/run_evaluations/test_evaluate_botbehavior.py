dcbb1ea2430ee97be25dbaa7e5384c4e0c825545
pragmatical <lunajorge@microsoft.com>
2024-09-11 13:59:03 +0000

    updatepath

--- evaluation/tests/integration/run_evaluations/test_evaluate_botbehavior.py
+++ evaluation/tests/integration/run_evaluations/test_evaluate_botbehavior.py
@@ -7,16 +7,17 @@ import pandas as pd
 from run_evaluations.evaluate_bot_behavior import evaluate_bot_behavior
 from promptflow.core import AzureOpenAIModelConfiguration
 
+
 class TestEvaluateBotBehavior(unittest.TestCase):
 
     def test(self):
-        datafilepath = "/workspaces/genai-drink-discovery/eval_flow/data_files/smoke_test/test_prompt.jsonl"
+        datafilepath = "/workspaces/botify/evaluation/data_files/bot_behavior.jsonl"
         model_config = AzureOpenAIModelConfiguration(
-                azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT_EVAL"),
-                api_key=os.environ.get("AZURE_OPENAI_API_KEY_EVAL"),
-                azure_deployment=os.environ.get("AZURE_OPENAI_MODEL_NAME_EVAL"),
-                api_version=os.environ.get("AZURE_OPENAI_API_VERSION"),
-            )
+            azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT_EVAL"),
+            api_key=os.environ.get("AZURE_OPENAI_API_KEY_EVAL"),
+            azure_deployment=os.environ.get("AZURE_OPENAI_MODEL_NAME_EVAL"),
+            api_version=os.environ.get("AZURE_OPENAI_API_VERSION"),
+        )
         result = evaluate_bot_behavior(
             dataset_path=datafilepath,
             model_config=model_config
